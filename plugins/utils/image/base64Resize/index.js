6c6c337142afce39219ee95cf74c8f32474ca261
TerryChan <terrylau@4d.com.hk>
2018-10-24 18:20:58 +0800

    no message

--- plugins/utils/image/base64Resize/index.js
+++ plugins/utils/image/base64Resize/index.js
@@ -54,22 +54,22 @@ class Base64ImageResizer{
     ** Get standard width height of image
     ** @param1: image path
     */
-    getStatWdithInfo(image) {
-    	return new Promise(resolve => {
-		    	// console.log('>>>>image>>>>>', image, sizeOf);
-		     //  	sizeOf(image, (err, size) => {
-		     //  		console.log('>>>>>>>>>', size);
-			    //     if(err) {
-			    //     	throw(err);
-			    //     }
-			    //     resolve(size);
-		    	// });
-		    setTimeout(() => {
-		    	console.log('> getStatWdithInfo');
-		    	resolve();
-		    }, 3000);
-		});
-    }
+  //   getStatWdithInfo(image) {
+  //   	return new Promise(resolve => {
+		//     	// console.log('>>>>image>>>>>', image, sizeOf);
+		//     sizeOf(image, (err, size) => {
+		//       	console.log('>>>>>>>>>', err, size);
+		// 	    if(err) {
+		// 	       throw(err);
+		// 	    }
+		// 	    resolve(size);
+		//    	});
+		//     // setTimeout(() => {
+		//     // 	console.log('> getStatWdithInfo');
+		//     // 	resolve();
+		//     // }, 3000);
+		// });
+  //   }
     /*
 	** calculate proportion of image
 	*/
@@ -105,32 +105,28 @@ class Base64ImageResizer{
     	if (existsSync(path)) {
     		try {
 	   //  		console.log('> start convert to base64 string: ', path);
-				const base64 = readFileSync(path, 'base64');
-				// if the file is request file stream, the size can be obtained
-				const size = originalSize || statSync(path).size;
-				console.log('> sizessssssss baseFiles: ', size);
-				console.log('>>>>>>>>>!>>>>>>>>');
-				const info = await this.getStatWdithInfo(path);
-				console.log('>>>>>>>>asdasdasdasdasfdfdf>');
-				// console.log('> info baseFiles: ', info);
-				// const optimize = this.getProportion(info);
-				// console.log('> optimizedddd baseFiles: ', optimize);
-				// const needCompress = this.isCompressedTobe(info, size);
-				// console.log('> needCompress baseFiles: ', needCompress);
-				// this._baseFiles = [ ...this._baseFiles, {
-				// 	file, 
-				//     base64: `${this._prefix}${base64}`,		// encoded base64 image string
-				//    	info,									// basic image info (e.g. width, height)
-				//    	size,									// original size of source image
-				//     needCompress,
-				//     optimize,								// optimized width and height against this.maxWidth
-				// }];
-			} catch (err) {
-				console.log('err: ', err);
-			}
+			const base64 = readFileSync(path, 'base64');
+			// if the file is request file stream, the size can be obtained
+			const size = originalSize || statSync(path).size;
+			console.log('> sizessssssss baseFiles: ', size);
+			console.log('>>>>>>>>>!>>>>>>>>');
+			const info = await promisify(sizeOf(path));
+			console.log('>>>>>>>>asdasdasdasdasfdfdf>');
+			// console.log('> info baseFiles: ', info);
+			// const optimize = this.getProportion(info);
+			// console.log('> optimizedddd baseFiles: ', optimize);
+			// const needCompress = this.isCompressedTobe(info, size);
+			// console.log('> needCompress baseFiles: ', needCompress);
+			// this._baseFiles = [ ...this._baseFiles, {
+			//		file, 
+			//    	base64: `${this._prefix}${base64}`,		// encoded base64 image string
+			//    	info,									// basic image info (e.g. width, height)
+			//    	size,									// original size of source image
+			//     	needCompress,
+			//     	optimize,								// optimized width and height against this.maxWidth
+			// }];
 			
 		}
-		console.log('>>>>>>>>>54fddsf');
     }
     /*
     ** Convert all of files stream to base64
@@ -160,13 +156,7 @@ class Base64ImageResizer{
 		if (this._files.length) {
 			// convert binary data to base64 encoded string
 			const infoTasks = _map(this._files, file => this.collectImageInfo(file));
-			console.log('>>>>>43434>>>>', infoTasks[0]);
-			try {
-				const result = await Promise.all(infoTasks);
-				console.log(result);
-			} catch (err) {
-				console.log('>>>>>>', err);
-			}
+			await Promise.all(infoTasks);
 			if (this._baseFiles.length) {
 		    	const { _baseFiles: baseFiles } = this;
 				this._baseFiles = _map(baseFiles, file => ({
@@ -175,7 +165,7 @@ class Base64ImageResizer{
 				}));
 				console.log('> baseFiles: ', this._baseFiles);
 			}
-			console.log('>>>>>>213123123>>>');
+
 		}
 	}
 }
