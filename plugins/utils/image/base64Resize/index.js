5059c32ebbf72abfc006697193b175808805bced
TerryChan <terrylau@4d.com.hk>
2018-10-24 12:28:17 +0800

    no message

--- plugins/utils/image/base64Resize/index.js
+++ plugins/utils/image/base64Resize/index.js
@@ -100,10 +100,10 @@ class Base64ImageResizer{
     async collectImageInfo({ path, size: originalSize }) {
     	if (existsSync(path)) {
     		console.log('> start convert to base64 string: ', path);
-			const base64 = fs.readFileSync(path, 'base64');
+			const base64 = readFileSync(path, 'base64');
 			console.log('> base64 baseFiles: ', base64);
 			// if the file is request file stream, the size can be obtained
-			const size = originalSize || fs.statSync(path).size;
+			const size = originalSize || statSync(path).size;
 			console.log('> size baseFiles: ', size);
 			const info = await this.getStatWdithInfo(path) || {};
 			console.log('> info baseFiles: ', info);
