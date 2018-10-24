c79c5bfb7c0611c0bcba91d9ad6f033e8e4a21f2
TerryChan <terrylau@4d.com.hk>
2018-10-24 16:05:13 +0800

    324234

--- plugins/utils/image/base64Resize/index.js
+++ plugins/utils/image/base64Resize/index.js
@@ -33,6 +33,7 @@ class Base64ImageResizer{
         this._maxWidth = maxWidth;
         this._maxSize = maxSize;
         this._maxHeight = maxHeight;
+        this.resizeBase64Images = this.resizeBase64Images.bind(this);
         // this._maxNo = maxNo;
         this._prefix = `data:image/${outputType};base64, `;
         // if (maxNo !=== -1 && files.length > maxNo) {
@@ -41,7 +42,6 @@ class Base64ImageResizer{
     }
     set files(files) {
     	this._files = !_isArray(files) ? [files] : files;
-    	this.convertToBase64();
     }
     get files() {
     	return this._baseFiles;
@@ -57,14 +57,18 @@ class Base64ImageResizer{
   //   getStatWdithInfo(image) {
   //   	return new Promise((resolve, reject) => {
 		//     try {
-		//     	console.log('>>>>image>>>>>', image, sizeOf);
-		//       	sizeOf(image, (err, size) => {
-		//       		console.log('>>>>>>>>>', size);
-		// 	        if(err) {
-		// 	        	throw(err);
-		// 	        }
-		// 	        resolve(size);
-		//     	});
+		//     	// console.log('>>>>image>>>>>', image, sizeOf);
+		//      //  	sizeOf(image, (err, size) => {
+		//      //  		console.log('>>>>>>>>>', size);
+		// 	    //     if(err) {
+		// 	    //     	throw(err);
+		// 	    //     }
+		// 	    //     resolve(size);
+		//     	// });
+		//     	setTimeout(() => {
+		//     		console.log('> getStatWdithInfo');
+		//     		resolve();
+		//     	})
 		//     } catch (err) {
 		//     	console.log('>>>>>err>>>>', err);
 		//     	reject(err);
@@ -130,21 +134,20 @@ class Base64ImageResizer{
 			}
 			
 		}
+		console.log('>>>>>>>>>54fddsf');
     }
     /*
     ** Convert all of files stream to base64
     */
-    async convertToBase64() {
-	    // read binary data
-	    if (this._files.length) {
-			// convert binary data to base64 encoded string
-			const infoTasks = _map(this._files, file => this.collectImageInfo(file));
-			console.log('>>>>>43434>>>>');
-			await Promise.all(infoTasks).catch(err => console.log('> convertToBase64: ', err));
-			console.log('>>>>>>213123123>>>');
-		}
+ //    async convertToBase64() {
+	//     // read binary data
+	//     if (this._files.length) {
+	// 		// convert binary data to base64 encoded string
+	// 		const infoTasks = _map(this._files, file => this.collectImageInfo(file));
+	// 		await Promise.all(infoTasks).catch(err => console.log('> convertToBase64: ', err));
+	// 	}
 		
-	}
+	// }
 
 	resizeBase64({ optimize, base64 }) {
 		if (base64 && optimize && optimize.width && optimize.height) {
@@ -158,13 +161,20 @@ class Base64ImageResizer{
 	** resize the base64 image with optimized info
 	*/
 	resizeBase64Images() {
-	    if (this._baseFiles.length) {
-	    	const { _baseFiles: baseFiles } = this;
-			this._baseFiles = _map(baseFiles, file => ({
-				...file,
-				path: this.resizeBase64(file),
-			}));
-			console.log('> baseFiles: ', this._baseFiles);
+		if (this._files.length) {
+			// convert binary data to base64 encoded string
+			const infoTasks = _map(this._files, file => this.collectImageInfo(file));
+			console.log('>>>>>43434>>>>');
+			await Promise.all(infoTasks).catch(err => console.log('> convertToBase64: ', err));
+			if (this._baseFiles.length) {
+		    	const { _baseFiles: baseFiles } = this;
+				this._baseFiles = _map(baseFiles, file => ({
+					...file,
+					path: this.resizeBase64(file),
+				}));
+				console.log('> baseFiles: ', this._baseFiles);
+			}
+			console.log('>>>>>>213123123>>>');
 		}
 	}
 }
